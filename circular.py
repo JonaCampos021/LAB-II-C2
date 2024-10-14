e50fb8f96750786a2ecca47af02ac053bb9f61ab
Samessi <eliasflores988@gmail.com>
2024-10-13 20:55:24 -0600

    Update circular.py

--- circular.py
+++ circular.py
@@ -18,5 +18,4 @@ plt.show()
 # 1. Proporciones Claras: El gráfico circular permite visualizar de manera efectiva las proporciones relativas de cada tipo de entrenamiento.
 # 2. Identificación de Tendencias: Un tipo de entrenamiento que ocupa la mayor parte del gráfico podría indicar una tendencia hacia ese estilo específico.
 # 3. Diversidad en los Workouts: Si varios tipos tienen porciones significativas, sugiere que los usuarios disfrutan de una variedad de actividades.
-# 4. Posibles Áreas de Mejora: Un tipo de workout con representación muy baja podría ser una oportunidad para introducir nuevas clases o promociones.
-# 5. Contexto Adicional: Complementar el análisis con información demográfica o de comportamiento de los usuarios podría ofrecer más insights.
+
